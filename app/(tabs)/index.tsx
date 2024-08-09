f66d9e1f2907b16540f7c9da1818381c519d6154
Gazel <gazel@ngoprekweb.com>
2024-08-09 20:34:11 +0700

    first init - adding NativeWind and tailwindcss - adding remixicon native - adding index.d.ts for importing images types

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,118 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, Platform, SafeAreaView, ScrollView, Text, View } from "react-native";
+import Icon from "react-native-remix-icon";
+import images from "@/constants/images";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function HomeScreen() {
+export default function Home() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView>
+      <ScrollView>
+        <View className="main py-8 ">
+          <View className="section-1 px-5 py-2">
+            <Text className="text-2xl font-semibold">Home</Text>
+          </View>
+          <View className="section-2 px-5 box-content gap-y-3 pb-3">
+            <View className="profile flex-row justify-between items-center py-1.5">
+              <View className="pp-name flex-row items-center gap-3">
+                <View className="pp bg-cyan-600 w-8 h-8 rounded-full justify-center items-center border">
+                  <Text className="text-white text-xs font-medium">EM</Text>
+                </View>
+                <Text className="text-sm font-bold">Euis Marlina</Text>
+              </View>
+              <Icon
+                name="settings-line"
+                size={20}
+                color="#1e40af"
+              ></Icon>
+            </View>
+            <View className="saldo px-3 py-2 bg-blue-800 rounded-lg flex-col gap-y-1 w-fit box-content">
+              <View className="flex-row justify-between">
+                <Text className="text-base font-bold text-white">Saldo Toko</Text>
+                <Icon
+                  name="more-2-fill"
+                  color="white"
+                  size={12}
+                ></Icon>
+              </View>
+              <View className="flex-row">
+                <Text className="text-xs font-bold text-white">Rp</Text>
+                <Text className="text-xl font-bold text-white">1.800.000,00</Text>
+              </View>
+              <View className="flex-row items-center">
+                <Text className="text-gray-100 opacity-50">Ubah Saldo</Text>
+                <Icon
+                  name="arrow-right-s-line"
+                  size={12}
+                  color="#899ad3"
+                ></Icon>
+              </View>
+            </View>
+          </View>
+          <View className="section-3 px-5 flex-col gap-y-2 ">
+            <Text className="text-sm font-bold text-gray-700">Stok/Harga Galon</Text>
+            <View className="stok-row-1 flex-row justify-between gap-2 ">
+              <View className="mini-box border-2 flex-col rounded-lg px-3 flex-1  gap-y-2 pb-2">
+                <View className="flex-row justify-between items-center ">
+                  <Text className="text-xs text-gray-700 font-semibold">Aqua</Text>
+                  <Icon
+                    name="more-2-fill"
+                    color="black"
+                    size={16}
+                  ></Icon>
+                </View>
+                <View className="flex-row justify-between ">
+                  <View>
+                    <Text className="stok text-2xl font-semibold text-gray-700">28</Text>
+                    <Text className="harga text-2xl font-semibold text-gray-700">/20K</Text>
+                  </View>
+                  <Image
+                    source={images.aqua}
+                    resizeMode="contain"
+                    className="w-14 h-16"
+                  />
+                </View>
+                <View className="flex-row items-center opacity-50">
+                  <Text className="text-gray-700 text-xs font-normal">Ubah Saldo</Text>
+                  <Icon
+                    name="arrow-right-s-line"
+                    size={12}
+                    color="#374151"
+                  ></Icon>
+                </View>
+              </View>
+              <View className="mini-box border-2 flex-col rounded-lg px-3 flex-1  gap-y-2 pb-2">
+                <View className="flex-row justify-between items-center">
+                  <Text className="text-xs text-gray-700 font-semibold">Aqua</Text>
+                  <Icon
+                    name="more-2-fill"
+                    color="black"
+                    size={12}
+                  ></Icon>
+                </View>
+                <View className="flex-row justify-between">
+                  <View>
+                    <Text className="stok text-2xl font-semibold text-gray-700">15</Text>
+                    <Text className="harga text-2xl font-semibold text-gray-700">/5K</Text>
+                  </View>
+                  <Image
+                    source={images.isiUlang}
+                    resizeMode="contain"
+                    className="w-14 h-16"
+                  />
+                </View>
+                <View className="flex-row items-center opacity-50">
+                  <Text className="text-gray-700 text-xs font-normal">Ubah Saldo</Text>
+                  <Icon
+                    name="arrow-right-s-line"
+                    size={12}
+                    color="#374151"
+                  ></Icon>
+                </View>
+              </View>
+              
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
---
+++ constants/images.ts
@@ -0,0 +1,4 @@
+import aqua from "../assets/images/aqua.png";
+import isiUlang from "../assets/images/isi-ulang.png";
+
+export default { aqua , isiUlang };
