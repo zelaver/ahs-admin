8ac3163c4970785c45ed7578a0c29ce9bf36ac16
Gazel <gazel@ngoprekweb.com>
2024-09-12 17:08:04 +0700

    fix: add custom style in DatePicker

--- components/DatePicker.tsx
+++ components/DatePicker.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Text, TouchableOpacity } from "react-native";
 import Icon from "react-native-remix-icon";
 
-const DatePicker = ({ date, setDate, onDateChange, customStyle = '' }) => {
+const DatePicker = ({ date, setDate, onDateChange, containerStyle = "", textStyle = "", iconColor = "" }) => {
   const showDateMode = (currentMode) => {
     DateTimePickerAndroid.open({
       value: date,
@@ -25,9 +25,9 @@ const DatePicker = ({ date, setDate, onDateChange, customStyle = '' }) => {
     <TouchableOpacity
       activeOpacity={0.5}
       onPress={showDatepicker}
-      className={`flex-row items-center justify-start rounded-md border border-blue-800 px-2 py-1 ${customStyle}`}>
-      <Icon name="calendar-line" size={16} color="#1e40af" />
-      <Text className={`ml-1 text-xs font-semibold text-blue-800`}>
+      className={`flex-row items-center justify-start rounded-md border border-blue-800 px-2 py-1 ${containerStyle}`}>
+      <Icon name="calendar-line" size={16} color={iconColor ? iconColor : "#1e40af"} />
+      <Text className={`ml-1 text-xs font-semibold text-blue-800 ${textStyle}`}>
         {date.toLocaleDateString() == new Date().toLocaleDateString() ? "Hari ini" : date?.toLocaleDateString()}
       </Text>
     </TouchableOpacity>
