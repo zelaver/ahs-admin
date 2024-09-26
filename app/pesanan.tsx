0bddabeb9e43f811cecc9ef64bf9a64819a1c90f
Gazel <gazel@ngoprekweb.com>
2024-09-26 12:22:44 +0700

    fix(pesanan): time input

--- app/pesanan.tsx
+++ app/pesanan.tsx
@@ -293,9 +293,9 @@ const BottomSheetAddPesanan = ({ bottomSheetModalRef }: any) => {
     const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() menghasilkan bulan dengan indeks mulai dari 0
     const day = String(date.getDate()).padStart(2, "0");
 
-    const hours = String(date.getHours()).padStart(2, "0");
-    const minutes = String(date.getMinutes()).padStart(2, "0");
-    const seconds = String(date.getSeconds()).padStart(2, "0");
+    const hours = String(new Date().getHours()).padStart(2, "0");
+    const minutes = String(new Date().getMinutes()).padStart(2, "0");
+    const seconds = String(new Date().getSeconds()).padStart(2, "0");
 
     const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
