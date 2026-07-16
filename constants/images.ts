68d96e313f27edeacd64e552162b3d215d2a2326
zelaver <gazel@binaridigital.tech>
2026-07-16 18:31:00 +0700

    fix(backup): add new image

--- constants/images.ts
+++ constants/images.ts
@@ -1,8 +1,9 @@
 import aqua from "../assets/images/aqua.png";
-import isiUlang from "../assets/images/isi-ulang.png";
+import backupHero from "../assets/images/backup-hero.png";
 import galonKosong from "../assets/images/galon-kosong.png";
 import gas12Kg from "../assets/images/gas-12-kg.png";
 import gasKosong from "../assets/images/gas-kosong.png";
-import logo from "../assets/images/logo.png"
+import isiUlang from "../assets/images/isi-ulang.png";
+import logo from "../assets/images/logo.png";
 
-export default { aqua , isiUlang, galonKosong, gas12Kg, gasKosong, logo };
+export default { aqua , isiUlang, galonKosong, gas12Kg, gasKosong, logo, backupHero };
